2f53404a1259a3ec371e8d087e359da71e19ffc1
olexitomaselli <olexitom29@gmail.com>
2023-09-19 12:29:34 +0200

    created components folder end two new componenst

---
+++ src/components/ThemeButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ThemeButton.css";
+
+export const ThemeButton = () => {
+  return (
+    <div class="form-check form-switch custom-switch">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckDefault"
+      />
+    </div>
+  );
+};
