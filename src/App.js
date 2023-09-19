1a6c333ba544305378267132588837be3ab4543d
olexitomaselli <olexitom29@gmail.com>
2023-09-19 13:03:05 +0200

    implemented useReducer for change card theme with switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import { Card } from "./components/Card";
-import { ThemeButton } from "./components/ThemeButton";
 
 function App() {
   return (
     <div className="App">
-      <ThemeButton />
       <Card />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Card.css";
 
+const initialState = {
+  isBlack: false,
+};
+
+const cardReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_COLOR":
+      console.log("Toggling color");
+      return {
+        ...state,
+        isBlack: !state.isBlack,
+      };
+    default:
+      return state;
+  }
+};
+
 export const Card = () => {
+  const [state, dispatch] = useReducer(cardReducer, initialState);
+  const cardColor = state.isBlack ? "black" : "white";
+  const toggleColor = () => dispatch({ type: "TOGGLE_COLOR" });
+
   return (
     <>
-      <div className="card">
+      <div
+        className="card"
+        style={{
+          backgroundColor: cardColor,
+          width: "200px",
+          height: "200px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">
@@ -21,6 +52,15 @@ export const Card = () => {
             Another link
           </a>
         </div>
+      </div>
+      <div class="form-check form-switch custom-switch">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckDefault"
+          onClick={toggleColor}
+        />
       </div>
     </>
   );
